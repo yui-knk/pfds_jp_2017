ae03809295134410c10dcd82066d1c920f3c4836
yui-knk <spiketeika@gmail.com>
2017-08-20 16:38:53 +0900

    Practice 5.4 (balanced smaller)

--- ch05/src/SplayHeap.hs
+++ ch05/src/SplayHeap.hs
@@ -10,12 +10,18 @@ isEmpty _ = False
 -- smaller includes elements which are euqal to pivot
 --
 -- smaller 5 (T (T (T E 1 E) 3 E) 4 (T (T E 5 E) 8 (T E 10 E)))
+-- smaller 8 (T E 1 (T E 2 (T E 3 (T E 4 (T E 5 (T E 6 (T E 7 E)))))))
 smaller :: Ord a => a -> SplayHeap a -> SplayHeap a
 smaller pivot E = E
 smaller pivot (T a x b) =
     if x > pivot
     then smaller pivot a
-    else T a x (smaller pivot b)
+    else case b of
+        E -> T a x E
+        (T a1 y a2) ->
+            if y > pivot
+            then T a x (smaller pivot a1)
+            else T (T a x a1) y (smaller pivot a2)
 
 
 -- bigger 0 (T (T (T (T (T (T (T E 1 E) 2 E) 3 E) 4 E) 5 E) 6 E) 7 E)
