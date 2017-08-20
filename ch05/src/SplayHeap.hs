576ad749c2ca7f8fc5840ff1bf32cc1021f6f530
yui-knk <spiketeika@gmail.com>
2017-08-20 16:13:11 +0900

    Balanced bigger

--- ch05/src/SplayHeap.hs
+++ ch05/src/SplayHeap.hs
@@ -11,12 +11,19 @@ isEmpty _ = False
 --smaller x E = E
 --smaller x t 
 
+
+-- bigger 0 (T (T (T (T (T (T (T E 1 E) 2 E) 3 E) 4 E) 5 E) 6 E) 7 E)
 bigger :: Ord a => a -> SplayHeap a -> SplayHeap a
 bigger pivot E = E
 bigger pivot (T a x b) =
     if x <= pivot
     then bigger pivot b
-    else T (bigger x a) x b
+    else case a of
+        E -> T E x b
+        (T a1 y a2) ->
+            if y <= pivot
+            then T (bigger pivot a2) x b
+            else T (bigger pivot a1) y (T a2 x b)
 
 --insert :: a -> SplayHeap a -> SplayHeap a
 --insert x t = T (smaller x t) x (bigger x t)
