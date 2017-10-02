29629ac509d26b3dd98fa0d2fb741d1ad77fb9e2
Yuichiro Kaneko <kaneko.yuichiro@moneyforward.co.jp>
2017-10-02 19:42:17 +0900

    Fix up

--- ch06/src/practice_6_1.rb
+++ ch06/src/practice_6_1.rb
@@ -4,14 +4,14 @@ require 'graphviz'
 gr = GraphViz.new(:G, type: :digraph)
 
 # Create nodes
-empty = gr.add_nodes("empty `[]`           (3)")
-a     = gr.add_nodes("a     `[0]`          (3)")
-b     = gr.add_nodes("b     `[0, 1]`       (3)")
+empty = gr.add_nodes("empty `[]`           (4)")
+a     = gr.add_nodes("a     `[0]`          (4)")
+b     = gr.add_nodes("b     `[0, 1]`       (4)")
 c     = gr.add_nodes("c     `[1]`          (2)")
 d     = gr.add_nodes("d     `[0, 1, 2]`    (2)")
-e     = gr.add_nodes("e     `[1, 0, 1, 2]` (0)")
-f     = gr.add_nodes("f     `[]`           (0)")
-g     = gr.add_nodes("g     `[0, 1, 2, 3]` (0)")
+e     = gr.add_nodes("e     `[1, 0, 1, 2]` (1)")
+f     = gr.add_nodes("f     `[]`           (1)")
+g     = gr.add_nodes("g     `[0, 1, 2, 3]` (1)")
 
 # Create an edge between the two nodes
 gr.add_edges(empty, a)
