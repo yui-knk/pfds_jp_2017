1351eeb95b692b7eb95dc1dec2be101b68b910f2
yui-knk <spiketeika@gmail.com>
2018-07-21 23:40:36 +0900

    Implement cons

--- ch10/src/NonUniform.hs
+++ ch10/src/NonUniform.hs
@@ -8,3 +8,13 @@ size (One x s) = 1 + 2 * (size s)
 --size (Zero (One (0) Nil)) -- this cause error
 --size (Zero (One (0, 1) Nil))
 --size (Zero (One (0, 1) (One ((2, 3), (4, 5)) Nil)))
+
+cons :: a -> Seq a -> Seq a
+cons x Nil = One x Nil
+cons x (Zero s) = One x s
+cons x (One y s) = Zero (cons (x, y) s)
+
+--cons 0 Nil
+--cons 1 (cons 0 Nil)
+--cons 2 (cons 1 (cons 0 Nil))
+--cons 3 (cons 2 (cons 1 (cons 0 Nil)))
