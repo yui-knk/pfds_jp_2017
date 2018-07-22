1722d2df8a5535e34881439b47552afa6b21b0b7
yui-knk <spiketeika@gmail.com>
2018-07-22 12:56:13 +0900

    Add some functions

--- ch10/src/NonUniform.hs
+++ ch10/src/NonUniform.hs
@@ -1,3 +1,5 @@
+import Prelude hiding (head, tail, lookup)
+
 data Seq a = Nil | Zero (Seq (a, a)) | One a (Seq (a, a)) deriving(Show)
 
 size :: Seq a -> Int
@@ -18,3 +20,31 @@ cons x (One y s) = Zero (cons (x, y) s)
 --cons 1 (cons 0 Nil)
 --cons 2 (cons 1 (cons 0 Nil))
 --cons 3 (cons 2 (cons 1 (cons 0 Nil)))
+
+uncons :: Seq a -> (a, Seq a)
+uncons (One x Nil) = (x, Nil)
+uncons (One x s) = (x, Zero s)
+uncons (Zero s) = (x, One y s')
+    where ((x, y), s') = uncons s
+
+head :: Seq a -> a
+head s = x
+    where (x, _) = uncons s
+
+tail :: Seq a -> Seq a
+tail s = s'
+    where (_, s') = uncons s
+
+lookup :: Int -> Seq a -> a
+lookup 0 (One x s) = x
+lookup i (One x s) = lookup (i-1) (Zero s)
+lookup i (Zero s) = if (mod i 2) == 0 then x else y
+    where (x, y) = lookup (div i 2) s
+
+update :: Int -> a -> Seq a -> Seq a
+update 0 e (One x s) = One e s
+update i e (One x s) = cons x (update (i-1) e (Zero s))
+update i e (Zero s) = Zero (update (div i 2) p s) -- it may be `(div i 2)` not `(i-1)`
+    where
+      (x, y) = lookup (div i 2) s
+      p = if (mod i 2) == 0 then (e, y) else (x, e)
